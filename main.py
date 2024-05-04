d0535f8aca7fab0bb83be8cc4db6b1d574f7bba7
Martiks <cappa.giuliano@gmail.com>
2024-05-04 10:51:59 -0300

    mover view_student_submenus a main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import math
 import msvcrt
 import sys
 
-from views.menus.student_submenu import view_student_submenus
 from views.student.view_students_profile import view_students_profile
 from views.student.view_update_student_profile import view_update_student_profile
 
@@ -213,6 +212,45 @@ def view_student_main_menu():
         )
 
     return option
+
+
+# Opciones de las secciones del menú principal de estudiantes
+# menu_option, submenu_option: string
+# invalid_submenu_option: boolean
+def view_student_submenus(menu_option):
+
+    print("\n")
+    match menu_option:
+        case "1":
+            print("a. Editar mis datos personales")
+            print("b. Eliminar mi perfil")
+            print("c. Volver")
+
+        case "2":
+            print("a. Ver candidatos")
+            print("b. Reportar un candidato")
+            print("c. Volver")
+
+        case "3":
+            print("a. Ver matcheos")
+            print("b. Eliminar un matcheo")
+            print("c. Volver")
+
+    submenu_option = input("\n¿Qué desea hacer? ")
+
+    invalid_submenu_option = (
+        submenu_option != "a" and submenu_option != "b" and submenu_option != "c"
+    )
+
+    while invalid_submenu_option:
+        print("La opción introducida no es válida.")
+        submenu_option = input("Por favor, introduzca una opción válida: ")
+
+        invalid_submenu_option = (
+            submenu_option != "a" and submenu_option != "b" and submenu_option != "c"
+        )
+
+    return submenu_option
 
 
 # Calculadora de valor de
